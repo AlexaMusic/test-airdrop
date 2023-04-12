19b2550a689c5e06022b65df5968712471a354d8
AlexaMusic <119608235+AlexaMusic@users.noreply.github.com>
2023-04-13 02:43:25 +0500

    hmm

--- app.py
+++ app.py
@@ -29,12 +29,11 @@ def transfer_airdrops():
         signed_tx = account.signTransaction(tx)
         tx_hash = w3.eth.sendRawTransaction(signed_tx.rawTransaction)
         print(f'Transferred {balance} Wei to {destination_address} (Tx Hash: {tx_hash.hex()})')
-        
+
 print("Deployment successful!")
 while True:
     latest_block = w3.eth.getBlock('latest')
     for tx_hash in latest_block['transactions']:
         if is_airdrop(tx_hash):
             transfer_airdrops()
-    latest_block_hex = hex(latest_block.number)
-    w3.eth.waitForTransactionReceipt(latest_block_hex)
+    w3.eth.waitForTransactionReceipt(latest_block['hash'])
